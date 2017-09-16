aba6726535b409a6c7e1040da110cd110e6aab44
Jeff Wainwright <yowainwright@gmail.com>
2017-09-16 15:14:09 -0700

    [init] modernize repo—slim update

--- js/src/RITTE.Information.js
+++ js/src/RITTE.Information.js
@@ -15,47 +15,47 @@
 		mainColor : {
 			armygreen : {
 				color_content : "Our Army Green is a bit brighter and more Lime-hued than what you see painted on old Jeeps, but it’s still a color not often associated with speed and grace. This juxtaposition is what makes a color like Army Green so stunning on a beautiful, lithe form like the Ace. Choose this, with a muted secondary color and have the most unique and stunning bike in town.",
-				image : "http://rittepaint.com/assets/images/white_bk.jpg"
+				image : "/assets/images/white_bk.jpg"
 			},
 			black : {
 				color_content : "For you, only black is the new black.",
-				image : "http://rittepaint.com/assets/images/white_bk.jpg"
+				image : "/assets/images/white_bk.jpg"
 			},
 			classicpink : {
 				color_content : "",
-				image : "http://rittepaint.com/assets/images/black_bk.jpg"
+				image : "/assets/images/black_bk.jpg"
 			},
 			darknavy : {
 				color_content : "Deep and complex, Dark Navy Blue is one of the most beautiful and unusual colors you can choose. Few bikes are painted this color, but when you see one it’s hard to look away. Often confused with black, Dark Navy is sophisticated and yet unassuming, and goes well with reds, oranges, whites and other blues.",
-				image : "http://rittepaint.com/assets/images/orange_bk.jpg"
+				image : "/assets/images/orange_bk.jpg"
 			},
 			deepred : {
 				color_content : "Bold yet not brash, aristocratic yet grounded, Deep Red is one of the most traditional colors of speed, victory and battle. A Deep Red Ace will go well with the 1965 Alfa Romeo Tipo 33 you probably have parked in the garage in your villa. It will pair well with white, or light blue accents.",
-				image : "http://rittepaint.com/assets/images/grass_bk.jpg"
+				image : "/assets/images/grass_bk.jpg"
 			},
 			gullgray : {
 				content : "Gray is not gray: it’s not nondescript or uncommitted or plain. Gray is both tough and beautiful. Among bikes, gray is pretty unusual and the perfect canvas for more bold accents.",
-				image : "http://rittepaint.com/assets/images/dark_blue_bk.jpg"
+				image : "/assets/images/dark_blue_bk.jpg"
 			},
 			italianred : {
 				color_content : "",
-				image : "http://rittepaint.com/assets/images/black_bk.jpg"
+				image : "/assets/images/black_bk.jpg"
 			},
 			ritteblue : {
 				color_content : "This is our original Ritte Blue, but with about 15% black, which brings out a lot of complex greens and grays. Esprit Blue is a bit meaner and more serious than a traditional baby blue and is unique among bike colors. It can trick the eye, with some people seeing it as gray and some as blue.",
-				image : "http://rittepaint.com/assets/images/orange_bk.jpg"
+				image : "/assets/images/orange_bk.jpg"
 			},
 			powderblue : {
 				color_content : "The original light blue we used for many years. Sky Blue has a long history as an accent to speed and taste. It’s a little known fact that the ancient Atlantans worshiped the sky god by painting themselves entirely in this color and racing each other on foot through cheetah dens until only one participant remained alive. The victor was then sacrificed to the Sky God, which is why it’s called Sky Blue.",
-				image : "http://rittepaint.com/assets/images/yellow_bk.jpg"
+				image : "/assets/images/yellow_bk.jpg"
 			},
 			slategray : {
 				color_content : "Possibly the meanest choice this side of matte black, Slate Gray is a dark stranger who prowls the streets at night seeking vigilante justice. Slate Gray will go with any secondary color, it’s just a matter if you want to go with a bold contrast or stick with the sinister dark theme.",
-				image : "http://rittepaint.com/assets/images/aqua_bk.jpg"
+				image : "/assets/images/aqua_bk.jpg"
 			},
 			white : {
 				color_content : "Minimalism and harmony are your mantras of choice and there’s a high likelihood your floors are so clean you could eat raw sushi off them. White is a traditional frame color, but still rarely seen. White looks great with any secondary color, but especially refined when paired with other lighter-hues.",
-				image : "http://rittepaint.com/assets/images/black_bk.jpg"
+				image : "/assets/images/black_bk.jpg"
 			},
 			nobg : {
 				image : ""
